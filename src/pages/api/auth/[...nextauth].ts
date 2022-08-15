a6e077ee365fd8b326f82bb4b189d218218d86b7
Pham Minh Hieu <hieupm@vnext.com.vn>
2022-08-15 17:25:02 +0700

    wip: next-auth credentials provider

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,10 +1,12 @@
 import NextAuth, { type NextAuthOptions } from "next-auth";
-import DiscordProvider from "next-auth/providers/discord";
+import CredentialsProvider from "next-auth/providers/credentials";
 
 // Prisma adapter for NextAuth, optional and can be removed
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
 import { prisma } from "../../../server/db/client";
 import { env } from "../../../env/server.mjs";
+import { trpc } from "../../../utils/trpc";
+import { User } from "@prisma/client";
 
 export const authOptions: NextAuthOptions = {
   // Include user.id on session
@@ -19,11 +21,17 @@ export const authOptions: NextAuthOptions = {
   // Configure one or more authentication providers
   adapter: PrismaAdapter(prisma),
   providers: [
-    DiscordProvider({
-      clientId: env.DISCORD_CLIENT_ID,
-      clientSecret: env.DISCORD_CLIENT_SECRET,
+    CredentialsProvider({
+      credentials: {
+        username: { label: "Username", type: "text", placeholder: "john.doe" },
+        password: { label: "Password", type: "password" },
+      },
+      async authorize(credentials, req) {
+        const {} = trpc.useQuery(["auth.login"]);
+        // console.log(credentials, req);
+        return {} as User;
+      },
     }),
-    // ...add more providers here
   ],
 };
 
