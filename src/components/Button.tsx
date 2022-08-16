8936e54adedd30dcc65873ac6718bb8d10406d14
Pham Minh Hieu <hieupm@vnext.com.vn>
2022-08-16 11:06:34 +0700

    basic header

---
+++ src/components/Button.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+type HTMLButtonProps = React.DetailedHTMLProps<
+  React.ButtonHTMLAttributes<HTMLButtonElement>,
+  HTMLButtonElement
+>;
+
+export const Button = React.forwardRef<HTMLButtonElement, HTMLButtonProps>(
+  function Button({ className, children, ...props }, ref) {
+    return (
+      <button
+        ref={ref}
+        className={`h-8 px-3 rounded border border-slate-300 bg-slate-50 text-neutral-800 hover:border-slate-500 hover:bg-slate-100 hover:text-neutral-900 ${
+          className || ""
+        }`}
+        {...props}
+      >
+        {children}
+      </button>
+    );
+  }
+);
